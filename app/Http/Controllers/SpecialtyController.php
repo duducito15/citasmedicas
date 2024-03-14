c2c8dc890b8c2df4115a7764df322239026e0ee5
Eduardo <echavez@utea.edu.pe>
2024-03-14 08:43:00 -0500

    Index especialidades

--- app/Http/Controllers/SpecialtyController.php
+++ app/Http/Controllers/SpecialtyController.php
@@ -6,6 +6,9 @@ use Illuminate\Http\Request;
 
 class SpecialtyController extends Controller
 {
+    public function __construct() {
+        $this->middleware('auth');
+    }
     function index() {
         return view('specialties.index');
     }
